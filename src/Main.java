9e0abbf8347f261602894e3a2b0df78927b1b98d
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-12-09 18:13:24 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		double[] vect = new double[N];
+
+		for (int i = 0; i < N; i++) {
+			vect[i] = sc.nextDouble();
+		}
+
+		double sum = 0.0;
+		for (int i = 0; i < N; i++) {
+			sum += vect[i];
+		}
+		double avg = sum / N;
+
+		System.out.printf("AVERAGE HEIGHT: %.2f%n", avg);
+		sc.close();
+	}
+}
